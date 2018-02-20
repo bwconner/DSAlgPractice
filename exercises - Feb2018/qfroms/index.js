c3c5045167fb3648b51a8b3d7fa49ac04a5c6471
Bruce Conner <bruce.conner.ii@gmail.com>
2018-02-19 23:15:36 -0500

    DSAlg Practice - Queue With Stacks
    Implemented a queue using two seperate stacks to maintain Queue
    expectations such as FIFO and what should be viewed when peeked. This
    meant having to pass elements back and forth between to stacks to get to
    the first element of a stack via a series of pops while maintaing
    correct order when placing all of the elements back into the first
    stack.

--- exercises - Feb2018/qfroms/index.js
+++ exercises - Feb2018/qfroms/index.js
@@ -14,6 +14,44 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+	constructor() {
+		this.stackA = new Stack;
+		this.stackB = new Stack;
+	}
+
+	add(data) {
+		this.stackA.push(data);
+	}
+
+	remove() {
+		while (this.stackA.peek()) {
+			this.stackB.push(this.stackA.pop());
+		}
+
+		var toRemove = this.stackB.pop(); //Pop off top element of stack B that would normally be the first element of a queue
+
+		while (this.stackB.peek()) {
+			this.stackA.push(this.stackB.pop());
+		}
+
+		return toRemove;
+
+	}
+
+	peek() {
+		while (this.stackA.peek()) {
+			this.stackB.push(this.stackA.pop());
+		}
+
+		var view = this.stackB.peek(); //Peek the top element of stack B that would normally be the first element of a queue
+
+		while (this.stackB.peek()) {
+			this.stackA.push(this.stackB.pop());
+		}
+
+		return view;
+	}
+}
 
 module.exports = Queue;
