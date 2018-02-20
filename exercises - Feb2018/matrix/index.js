737052e9fc696711cfd7ac60a2d3ea0f2433759f
Bruce Conner <bruce.conner.ii@gmail.com>
2018-02-19 23:00:49 -0500

    DSAlg Practice - Spiral Matrix Exercise
    Created a function that given a value create a 2D array that will spiral
    the values of 1 to n around the outside till it finish on the inside of
    the 2D array.

--- exercises - Feb2018/matrix/index.js
+++ exercises - Feb2018/matrix/index.js
@@ -15,6 +15,56 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+	var results = [];
+	
+	//Creates the number of arrays needed based on n
+	//And pushes them into our blank results array
+	for (var i = 0; i < n; i++) {
+		results.push([]);
+	}
+
+	var counter = 1;
+	var startColumn = 0;
+	var endColumn = n - 1;
+	var startRow = 0;
+	var endRow = n - 1;
+
+	while (startColumn <= endColumn && startRow <= endRow) {
+		//Top Row
+		for (var i = startColumn; i <= endColumn; i++) {
+			results[startRow][i] = counter;
+			counter++;
+		}
+
+		startRow++; //Move the start row down one
+
+		//Right hand side going down
+		for (var i = startRow; i <= endRow; i++) {
+			results[i][endColumn] = counter;
+			counter++;
+		}
+
+		endColumn--; //Move right hand side in one
+
+		//Bottom Row
+		for (var i = endColumn; i >= startColumn; i--) {
+			results[endRow][i] = counter;
+			counter++;
+		}
+
+		endRow--; //Move end row up one
+
+		//Left hand side going up
+		for (var i = endRow; i >= startRow; i--) {
+			results[i][startColumn] = counter;
+			counter++;
+		}
+
+		startColumn++; //Move left hand side in one column
+	}
+
+	return results;
+}
 
 module.exports = matrix;
