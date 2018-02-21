6249050c27bcfdd826bdf14d9754ad83f492bd44
Bruce Conner <bruce.conner.ii@gmail.com>
2018-02-20 22:35:07 -0500

    DSAlg Practice - Width of Tree
    Used a breadth first traversal to determine the widths of each row
    within a tree.

--- exercises - Feb2018/levelwidth/index.js
+++ exercises - Feb2018/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+	var answer = [0];
+	var tree = [root, 'eor'];
+
+	while (tree.length > 1) {
+		var node = tree.shift();
+		if (node === 'eor') {
+			answer.push(0);
+			tree.push('eor');
+		} else {
+			tree.push(...node.children);
+			answer[answer.length - 1]++;
+		}
+	}
+
+	return answer;
+}
 
 module.exports = levelWidth;
