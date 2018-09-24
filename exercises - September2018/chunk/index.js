3326ed7a04067ab3d89018287fee13670efd0617
Bruce Conner <bruce.conner.ii@gmail.com>
2018-09-23 21:02:18 -0700

    Chunked Array
    SOlutions for array of chunks.

--- exercises - September2018/chunk/index.js
+++ exercises - September2018/chunk/index.js
@@ -8,6 +8,32 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+
+	var chunckedArray = [];
+	var chunk = [];
+
+	var i = 0;
+	var j = 0;
+
+	// Loop through entire array of elements
+	while (i < array.length) {
+		// Iterate through sub-sections of array based on size given
+		// Create small chunk arrays of given size
+		for (k = 0; k < size; k++) {
+			if (array[k + i] !== undefined) {
+				chunk[k] = array[k + i];
+			}
+		}
+		// Put small chunk arrays into parent array
+		chunckedArray[j] = chunk;
+		i += size;
+		j++;
+		chunk = [];
+	}
+
+	return chunckedArray;
+
+}
 
 module.exports = chunk;
