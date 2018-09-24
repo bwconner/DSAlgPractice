84d5b5e0c231b0e2d648bf4eee9288996ff6ff57
Bruce Conner <bruce.conner.ii@gmail.com>
2018-09-23 20:27:14 -0700

    Reverse Integer
    Reversing an integer solution.

--- exercises - September2018/reverseint/index.js
+++ exercises - September2018/reverseint/index.js
@@ -8,6 +8,15 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+	let sign = n < 0 ? '-' : '';
+
+	if (n < 0) {
+		n = n * -1;
+	}
+
+	n = n.toString().split('').reverse().join('');
+	return parseInt(sign += n);
+}
 
 module.exports = reverseInt;
