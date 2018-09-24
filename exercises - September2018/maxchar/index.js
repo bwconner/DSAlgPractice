a8f7ed90d52a6c574d27c20be6fec83827b742f6
Bruce Conner <bruce.conner.ii@gmail.com>
2018-09-23 20:36:39 -0700

    Max Char
    Solution for finding char used the most in a string

--- exercises - September2018/maxchar/index.js
+++ exercises - September2018/maxchar/index.js
@@ -5,6 +5,30 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+	let charMap = {};
+
+	//Create JS Object holding the count of each character
+	for (var char of str) {
+		if (charMap[char]) {
+			charMap[char] = charMap[char] + 1;
+		} else {
+			charMap[char] = 1;
+		}
+	}
+
+
+	let maxChar = '';
+	let maxCount = 0;
+
+	for (char in charMap) {
+		if (maxCount < charMap[char]) {
+			maxCount = charMap[char];
+			maxChar = char;
+		}
+	}
+
+	return maxChar;
+}
 
 module.exports = maxChar;
